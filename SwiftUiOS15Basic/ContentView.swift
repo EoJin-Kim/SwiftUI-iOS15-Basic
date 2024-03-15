a0662e8530281e37273663a22f4bde8a0a10a81c
linexy <linexy@naver.com>
2024-03-15 19:25:51 +0900

    Initial Commit

---
+++ SwiftUiOS15Basic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUiOS15Basic
+//
+//  Created by 김어진 on 3/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUiOS15Basic/SwiftUiOS15BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiOS15BasicApp.swift
+//  SwiftUiOS15Basic
+//
+//  Created by 김어진 on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiOS15BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
